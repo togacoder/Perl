084abb051cf0a7570d3419c73e362db8b8d25a8a
togaerror <togaerror@gmail.com>
2017-12-28 15:29:00 +0900

    using .pm

---
+++ pmtest/testpm.pm
@@ -0,0 +1,8 @@
+package testpm;
+
+sub func1 {
+	print "Hello!\n";
+}
+
+1;
+__END__
---
+++ pmtest/usepm.pl
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use lib './'; # ここにtestpm.pmのpath
+require 'testpm.pm'; 
+
+testpm::func1();
+
+exit;
