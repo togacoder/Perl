47010001d4e5df9d1d8b2adcb77c1067971891ce
togaerror <togaerror@gmail.com>
2017-12-30 18:09:20 +0900

    配信者のListを手動で作った。

--- scrape_opgg.pl
+++ scrape_opgg.pl
@@ -9,14 +9,23 @@ my $scraper = scraper {
 			'name[]' => 'TEXT');
 };
 
-my $url = 'http://jp.op.gg/summoner/userName=togaerror';
+print "userName: ";
+my $userName = <>; chomp $userName;
+print "--wait--\n";
+my $url = 'http://jp.op.gg/summoner/userName=' . $userName;
 my $uri = new URI($url);
-
 my $res = $scraper->scrape($uri);
 
+my %hash;
 foreach (@{$res->{name}}) {
-	if($_ eq 'dyuma') {
-		print encode_utf8($_) . "\n";
+	$hash{$_}++;
+}
+
+my @userList = ('ZouSenpai', 'ぽよ氏', 'saruma1031', 'YwHALwY', 'Boosted Support', 'ｍｃｃｎ', 't4pu', 'make it', 'ソリアゲ王者ハサキング', 'うらぷろＨＪＭ', 'コリアンオッパ', 'るっかるん', 'るっかだゆ', 'MilkyBerry', 'Rivelia', 'kokou', '岸下コロンビア', 'Amy to Niv', 'John Doe', '睡魔ちゃん', 'IO', '月影まゆ', 'silent or mute', 'King of Player', 'けんかはやめろ', 'Sailor Saturn', 'demichaaan', 'とろぴ', 'EmilieDeRochefor', 'kotosinokabu', 'KTAP', 'AtTwilight', 'Purimichang', 'Pokémon Master', 'Asesinato', 'Timmy', 'Qcoco', 'marukatsu', 'Kurl', 'bunyaa');
+
+foreach my $key (@userList) {
+	if((defined $hash{$key})and(0 < $hash{$key})) {
+		print encode_utf8 "$hash{$key}\n";
 	}
 }
 
