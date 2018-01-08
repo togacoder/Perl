7dd7fb8579e4ccd539a43dd5a8ee63c20f620eec
togaerror <togaerror@gmail.com>
2018-01-08 11:14:43 +0900

    表示を変更

--- scrape_opgg.pl
+++ scrape_opgg.pl
@@ -9,10 +9,10 @@ my $scraper = scraper {
 			'name[]' => 'TEXT');
 };
 
-print "userName: ";
-my $userName = <>; chomp $userName;
-print "--wait--\n";
-my $url = 'http://jp.op.gg/summoner/userName=' . $userName;
+print "summoner name: ";
+my $summonerName = <>; chomp $summonerName;
+print "-- wait --\n";
+my $url = 'http://jp.op.gg/summoner/userName=' . $summonerName;
 my $uri = new URI($url);
 my $res = $scraper->scrape($uri);
 
@@ -21,9 +21,15 @@ foreach (@{$res->{name}}) {
 	$hash{$_}++;
 }
 
-my @userList = ('ZouSenpai', 'ぽよ氏', 'saruma1031', 'YwHALwY', 'Boosted Support', 'ｍｃｃｎ', 't4pu', 'make it', 'ソリアゲ王者ハサキング', 'うらぷろＨＪＭ', 'コリアンオッパ', 'るっかるん', 'るっかだゆ', 'MilkyBerry', 'Rivelia', 'kokou', '岸下コロンビア', 'Amy to Niv', 'John Doe', '睡魔ちゃん', 'IO', '月影まゆ', 'silent or mute', 'King of Player', 'けんかはやめろ', 'Sailor Saturn', 'demichaaan', 'とろぴ', 'EmilieDeRochefor', 'kotosinokabu', 'KTAP', 'AtTwilight', 'Purimichang', 'Pokémon Master', 'Asesinato', 'Timmy', 'Qcoco', 'marukatsu', 'Kurl', 'bunyaa');
+print "-- get summoners --\n";
+foreach my $key (keys %hash) {
+	print encode_utf8 "$key: $hash{$key}\n";
+}
 
-foreach my $key (@userList) {
+my @summonerList = ('ZouSenpai', 'ぽよ氏', 'saruma1031', 'YwHALwY', 'Boosted Support', 'ｍｃｃｎ', 't4pu', 'make it', 'ソリアゲ王者ハサキング', 'うらぷろＨＪＭ', 'コリアンオッパ', 'るっかるん', 'るっかだゆ', 'MilkyBerry', 'Rivelia', 'kokou', '岸下コロンビア', 'Amy to Niv', 'John Doe', '睡魔ちゃん', 'IO', '月影まゆ', 'silent or mute', 'King of Player', 'けんかはやめろ', 'Sailor Saturn', 'demichaaan', 'とろぴ', 'EmilieDeRochefor', 'kotosinokabu', 'KTAP', 'AtTwilight', 'Purimichang', 'Pokémon Master', 'Asesinato', 'Timmy', 'Qcoco', 'marukatsu', 'Kurl', 'bunyaa');
+
+print "-- match summoners --\n";
+foreach my $key (@summonerList) {
 	if((defined $hash{$key})and(0 < $hash{$key})) {
 		print encode_utf8 "$hash{$key}\n";
 	}
